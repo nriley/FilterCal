0ea669d84ce16065e9f09ae2aebb490a0c333f47
Nicholas Riley <nriley@sabi.net>
2016-08-16 08:55:00 -0400

    Add __all__ to fetch.py.

--- fetch.py
+++ fetch.py
@@ -1,9 +1,21 @@
 import filter
-
 import requests
+import urlparse
+
+__all__ = ('add_alarms_to_events_in_ics',)
 
 def add_alarms_to_events_in_ics(url):
+    parsed_url = urlparse.urlparse(url)
+
+    # validate that URL is valid
+    
+    assert parsed_url.scheme.lower() == 'https'
+    assert parsed_url.netloc.lower().endswith('new-innov.com')
+    
     response = requests.get(url)
+    assert response.headers['Content-Type'] == 'text/calendar'
+
+    # XXX multiple matching strings (weekend rounds, too)
     return filter.add_alarms_to_events_matching(response.text,
                                                 'Volume Back Up/ Home Call')
 
@@ -11,5 +23,3 @@ if __name__ == '__main__':
     import sys
     url = sys.argv[1]
     sys.stdout.write(add_alarms_to_events_in_ics(url))
-
-# XXX if error, redirect to the original URL
